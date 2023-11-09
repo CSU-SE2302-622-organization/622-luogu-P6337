0cd8795ed8fffdf866c326f73a0ce9ccf17d18a7
unknown <1303849129@qq.com>
2023-11-09 13:04:41 +0800

    1304

---
+++ 新建文件夹/CRNE，/CRNE，/CRNE，.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    if (n == 1)cout << 2 << endl;
+    else if (n % 2 == 0)cout << (n / 2 + 1) * (n / 2 + 1) << endl;
+    else cout << ((n + 1) / 2 + 1) * ((n - 1) / 2 + 1) << endl;
+    return 0;
+}
